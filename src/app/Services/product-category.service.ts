a8549cdec00ab57242eefa00014adf1e994ef219
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-07-09 17:58:52 -0600

    Adding ProductCategory service

---
+++ src/app/Services/product-category.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+//Importing
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //allows to receive the api response
+import { environment } from 'src/environments/environment'; //where I have the API URL
+import { ResponseApi } from '../Interfaces/response-api'; // allows us to receive our http response
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class MenuService {
+  private urlApi: string = `${environment.endPoint}ProductCategory/`;  //the user endpoint
+  constructor(private http: HttpClient) { } //dependency injection of http client 
+
+  List(): Observable<ResponseApi> {
+
+    return this.http.get<ResponseApi>(`${this.urlApi}List`); //the final endpoint 
+  }
+}
