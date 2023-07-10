1e303398a266d604b12589fe7249e794688c5168
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-07-09 18:29:44 -0600

    Adding Sale service

---
+++ src/app/Services/sale.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+//Importing
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs'; //allows to receive the api response
+import { environment } from 'src/environments/environment'; //where I have the API URL
+import { ResponseApi } from '../Interfaces/response-api'; // allows us to receive our http response
+import { Sale } from '../Interfaces/sale';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SaleService {
+  private urlApi: string = `${environment.endPoint}Sale/`;  //the user endpoint
+  constructor(private http: HttpClient) { } //dependency injection of http client 
+
+  //
+  Create(request: Sale): Observable<ResponseApi> { //RESPONSEAPI is the response structure I declared
+
+    return this.http.post<ResponseApi>(`${this.urlApi}Create`, request); //the final endpoint
+  }
+
+  History(searchBy: string, saleNumber: string, startDate: string, endDate: string): Observable<ResponseApi> { //RESPONSEAPI is the response structure I declared
+
+    return this.http.get<ResponseApi>(`${this.urlApi}SaleHistory?searchBy=${searchBy}&saleNumber=${saleNumber}&startDate=${startDate}&endDate=${endDate}`); //the final endpoint
+  }
+
+  Report(startDate: string, endDate: string): Observable<ResponseApi> { //RESPONSEAPI is the response structure I declared
+
+    return this.http.get<ResponseApi>(`${this.urlApi}Report?startDate=${startDate}&endDate=${endDate}`); //the final endpoint
+  }
+}
