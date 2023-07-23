899054f1a9a266e88b6ff6245dd0482bc565cfdc
Hellentorresr <htorres1@ucenfotec.ac.cr>
2023-07-22 20:18:51 -0600

    Adding authorizations

--- src/app/Components/layout/layout-routing.module.ts
+++ src/app/Components/layout/layout-routing.module.ts
@@ -7,17 +7,18 @@ import { ProductComponent } from './Pages/product/product.component';
 import { SaleHistoryComponent } from './Pages/sale-history/sale-history.component';
 import { ReportComponent } from './Pages/report/report.component';
 import { SaleComponent } from './Pages/sale/sale.component';
+import { rolGuard } from 'src/app/rol.guard';
 
 const routes: Routes = [{
   path: '',
   component: LayoutComponent, //the parent component
   children: [
-    { path: 'dashboard', component: DashboardComponent }, //url name and then the component
-    { path: 'users', component: UserComponent },
-    { path: 'products', component: ProductComponent },
-    { path: 'sales', component: SaleComponent },
-    { path: 'sales_history', component: SaleHistoryComponent },
-    { path: 'reports', component: ReportComponent },
+    { path: 'dashboard', component: DashboardComponent, data: { expectedRole: 'Admin' } }, //url name and then the component
+    { path: 'users', component: UserComponent, canActivate: [rolGuard], data: { expectedRole: 'Admin' } },
+    { path: 'products', component: ProductComponent, canActivate: [rolGuard], data: { expectedRole: ['Admin', 'Supervisor'] } },
+    { path: 'sales', component: SaleComponent, canActivate: [rolGuard], data: { expectedRole: ['Admin', 'Supervisor', 'Employee'] } },
+    { path: 'sales_history', component: SaleHistoryComponent, canActivate: [rolGuard], data: { expectedRole: ['Admin', 'Supervisor', 'Employee'] } },
+    { path: 'reports', component: ReportComponent, canActivate: [rolGuard], data: { expectedRole: ['Admin', 'Supervisor'] } },
   ]
 }];
 
